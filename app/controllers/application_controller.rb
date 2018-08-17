c69a283344a5bc7aa30b61be15e7958b3d0837c8
sthai1721 <sophia.thai@rutgers.edu>
2018-08-17 13:00:06 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,27 @@
 class ApplicationController < Sinatra::Base
-
+  
+  APP_ROOT = File.expand_path("..", __dir__)
+  
   configure do
-    set :public_folder, 'public'
+    set :public_folder, File.join(APP_ROOT, "app", "public")
     set :views, 'app/views'
   end
+  
+  get '/' do
+    erb :home
+  end
+  
+  get '/about_us' do
+    erb :about_us
+  end
 
+  get '/developers' do
+    erb :developers
+  end
+  
+  get '/artist' do
+    #puts params[:genre]
+    erb :artist
+  end
 end
+
---
+++ app/models/artists.rb
@@ -0,0 +1,451 @@
+class Artists 
+  
+  
+  
+# artist hash 
+# integer => name, spotify id, insta, twitter
+
+def all
+
+all_artists=
+[
+
+  {
+    "genre" => "country",
+    "name" => "Maren Morris", 
+    "id" => "6WY7D3jk8zTrHtmkqqo5GI", 
+    "insta" => "marenmorris", 
+    "twitter" => "MarenMorris",
+    "song" => "My Church"
+  },
+
+
+  {
+    "genre" => "country",
+    "name" => "Kelsea Ballerini", 
+    "id" => "3RqBeV12Tt7A8xH3zBDDUF", 
+    "insta" => "kelseaballerini", 
+    "twitter" => "KelseaBallerini",
+    "song" => "Legends"
+  },
+
+{
+  "genre" => "country",
+  "name" => "Miranda Lambert", 
+  "id" => "66lH4jAE7pqPlOlzUKbwA0", 
+  "insta" => "mirandalambert", 
+  "twitter" => "mirandalambert",
+  "song" => "To Learn Her"
+},
+
+{
+  "genre" => "country",
+  "name" => "Dixie Chicks", 
+  "id" => "25IG9fa7cbdmCIy3OnuH57", 
+  "insta" => "dixie_chicks", 
+  "twitter" => "dixiechicks",
+  "song" => "Goodbye Earl"
+},
+
+
+{
+  "genre" => "country",
+  "name" => "Dolly Parton", 
+  "id" => "32vWCbZh0xZ4o9gkz4PsEU", 
+  "insta" => "dollyparton", 
+  "twitter" => "DollyParton",
+  "song" => "Jolene"
+},
+
+
+{
+  "genre" => "country",
+  "name" => "Faith Hill", 
+  "id" => "25NQNriVT2YbSW80ILRWJa", 
+  "insta" => "faithhill", 
+  "twitter" => "faithhill",
+  "song" => "The Rest of Our Lives"
+},
+
+{
+  "genre" => "country",
+  "name" => "Trisha Yearwood", 
+  "id" => "3XlIhgydjvC4EniPFZT20j", 
+  "insta" => "trishayearwood",  
+  "twitter" => "trishayearwood",
+  "song" => "I Need You"
+},
+
+{
+  "genre" => "country",
+  "name" => "Lauren Alaina", 
+  "id" => "1v3tdpIdBSW14rHUfiEVOv", 
+  "insta" => "laurenalaina",  
+  "twitter" => "Lauren_Alaina",
+  "song" => "Road Less Traveled"
+},
+
+{
+  "genre" => "country",
+  "name" => "Shania Twain", 
+  "id" => "5e4Dhzv426EvQe3aDb64jL", 
+  "insta" => "shaniatwain", 
+  "twitter" => "ShaniaTwain",
+  "song" => "Man! I Feel Like a Woman!"
+},
+
+{
+  "genre" => "country",
+  "name" => "RaeLynn", 
+  "id" => "5dMnvpYEfXvSexjwnSRH5n", 
+  "insta" => "raelynnofficial", 
+  "twitter" => "raelynn",
+  "song" => "Queens Don't"
+},
+
+{
+  "genre" => "country",
+  "name" => "Jillian Jacqueline", 
+  "id" => "5GDZ6xhBwk7Yja97CFLmV7", 
+  "insta" => "jillianjacqeline",  
+  "twitter" => "jillianjmusic",
+  "song" => "Hate Me"
+},
+
+{
+  "genre" => "country",
+  "name" => "Sara Evans", 
+  "id" => "7qvsLYsYP0MHD7jkdv6DAG", 
+  "insta" => "saraevansmusic",  
+  "twitter" => "saraevansmusic",
+  "song" => "Slow Me Down"
+},
+
+{
+  "genre" => "country",
+  "name" => "Carrie Underwood", 
+  "id" => "4xFUf1FHVy696Q1JQZMTRj", 
+  "insta" => "carrieunderwood",  
+  "twitter" => "carrieunderwood",
+  "song" => "The Champion"
+},
+
+{
+  "genre" => "pop",
+  "name" => "Sia", 
+  "id" => "5WUlDfRSoLAfcVSX1WnrxN", 
+  "insta" => "siamusic",  
+  "twitter" => "Sia",
+  "song" => "Cheap Thrills"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Ingrid Michaelson", 
+  "id" => "2vm8GdHyrJh2O2MfbQFYG0", 
+  "insta" => "ingridmichaelson",  
+  "twitter" => "ingridmusic",
+  "song" => "Girls Chase Boys"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Lorde", 
+  "id" => "163tK9Wjr9P9DmM0AVK7lm", 
+  "insta" => "lordemusic", 
+  "twitter" => "lorde",
+  "song" => "Tennis Court"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Julia Michaels", 
+  "id" => "0ZED1XzwlLHW4ZaG4lOT6m", 
+  "insta" => "juliamichaels",  
+  "twitter" => "juliamichaels",
+  "song" => "Issues"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Alessia Cara", 
+  "id" => "2wUjUUtkb5lvLKcGKsKqsR", 
+  "insta" => "alessiasmusic",  
+  "twitter" => "alessiacara",
+  "song" => "Scars to Your Beatufiul"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Anne-Marie", 
+  "id" => "1zNqDE7qDGCsyzJwohVaoX", 
+  "insta" => "annemarie", 
+  "twitter" => "annemarie",
+  "song" => "2002"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Emeli Sande", 
+  "id" => "7sfgqEdoeBTjd8lQsPT3Cy", 
+  "insta" => "emelisande", 
+  "twitter" => "emelisande",
+  "song" => "Clown"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Florence and the Machine", 
+  "id" => "1moxjboGR7GNWYIMWsRjgG", 
+  "insta" => "florenceandthemachine",  
+  "twitter" => "flo_tweet",
+  "song" => "Hunger"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Hayley Kiyoko", 
+  "id" => "3LjhVl7GzYsza1biQjTpaN", 
+  "insta" => "hayleykiyoko",  
+  "twitter" => "HayleyKiyoko",
+  "song" => "Molecules"
+},
+
+{
+  "genre" => "pop",
+  "name"=> "Birdy", 
+  "id" => "2WX2uTcsvV5OnS0inACecP", 
+  "insta" => "birdyinstagram", 
+  "twitter" => "birdy",
+  "song" => "Tee Shirt"
+},
+
+  {
+    "genre" => "indie",
+    "name"=> "Flint Eastwood", 
+    "id" => "4Gh0D04oCEVbnsx63LXUmT", 
+    "insta" => "flinteastwood", 
+    "twitter" => "flinteastwood", 
+    "song" => "Queen"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Japanese Breakfast", 
+    "id" => "7MoIc5s9KXolCBH1fy9kkw", 
+    "insta" => "jbrekkie", 
+    "twitter" => "Jbrekkie",
+    "song" => "Machinist"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Mitski", 
+    "id" => "1Uvw5JW1C0Z0rAWiwubAjd", 
+    "insta" => "mitskileaks", 
+    "twitter" => "mitskileaks",
+    "song" => "Your Best American Girl"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "AURORA", 
+    "id" => "1WgXqy2Dd70QQOU7Ay074N", 
+    "insta" => "auroramusic", 
+    "twitter" => "AURORAmusic",
+    "song" => "Running with the Wolves"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Taylor Janzen", 
+    "id" => "3rTVTBOBT7NPgXtI5oWyxc", 
+    "insta" => "taylorjanzenn", 
+    "twitter" => "taylorjanzenn",
+    "song" => "Stations"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Julien Baker", 
+    "id" => "12zbUHbPHL5DGuJtiUfsip", 
+    "insta" => "julienrbaker", 
+    "twitter" => "julienrbaker",
+    "song" => "Turn Out the Lights"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Gabrielle Aplin", 
+    "id" => "3w6zswp5THsSKYLICUbDTZ", 
+    "insta" => "gabrielleaplin", 
+    "twitter" => "GabrielleAplin",
+    "song" => "Waking Up Slow"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Jain", 
+    "id" => "2HHmvvSQ44ePDH7IKVzgK0", 
+    "insta" => "jainmusic", 
+    "twitter" => "Jainmusic",
+    "song" => "Come"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "The Regrettes", 
+    "id" => "67WNUxmM7y4WzHPAVzBu3E", 
+    "insta" => "theregrettesband", 
+    "twitter" => "regrettesband", 
+    "song" => "Come Through"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Maggie Rogers", 
+    "id" => "4NZvixzsSefsNiIqXn0NDe",
+    "insta" => "maggierogers", 
+    "twitter" => "maggierogers",
+    "song" => "Dog Years"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "St. Vincent", 
+    "id" => "7bcbShaqKdcyjnmv4Ix8j6",
+    "insta" => "st_vincent", 
+    "twitter" => "st_vincent",
+    "song" => "Los Ageless"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Lily Allen", 
+    "id" => "13saZpZnCDWOI9D4IJhp1f",
+    "insta" => "lilyallen", 
+    "twitter" => "lilyallen",
+    "song" => "Smile"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "First Aid Kit", 
+    "id" => "21egYD1eInY6bGFcniCRT1",
+    "insta" => "firstaidkitband",
+    "twitter" => "FirstAidKitBand",
+    "song" => "Fireworks"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Grimes", 
+    "id" => "053q0ukIDRgzwTr4vNSwab", 
+    "insta" => "grimes", 
+    "twitter" => "Grimezsz",
+    "song" => "Genesis"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Banks", 
+    "id" => "2xe8IXgCTpwHE3eA9hTs4n",
+    "insta" => "hernameisbanks", 
+    "twitter" => "hernameisbanks",
+    "song" => "Crowded Places"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Courntey Barnett", 
+    "id" => "4OOlG5eBXSkSAAEeKjJb5Y",
+    "insta" => "courtneymelba", 
+    "twitter" => "courtneymelba",
+    "song" => "Continental Breakfast"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Best Coast",
+    "id" => "5YkBrE0wF8cAlq3GCOw5Eu",
+    "insta" => "best_coast", 
+    "twitter" => "BestCoast",
+    "song" => "Feeling Ok"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "MUNA", 
+    "id" => "6xdRb2GypJ7DqnWAI2mHGn",
+    "insta" => "whereismuna", 
+    "twitter" => "whereisMUNA",
+    "song" => "I Know a Place"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "The Aces", 
+    "id" => "2AmfMGi3WZMxqFDHissIAe", 
+    "insta" => "theaces", 
+    "twitter" => "theacesofficial",
+    "song" => "Volcanic Love"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Clairo", 
+    "id" => "3l0CmX0FuQjFxr8SK7Vqag", 
+    "insta" => "clairo", 
+    "twitter" => "clairo",
+    "song" => "4EVER"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Alvvays", 
+    "id" => "3kzwYV3OCB010YfXMF0Avt",
+    "insta" => "alvvaysband", 
+    "twitter" => "alvvaysband",
+    "song" => "In Undertow"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Yeah Yeah Yeahs", 
+    "id" => "3TNt4aUIxgfy9aoaft5Jj2",
+    "insta" => "yeahyeahyeahs", 
+    "twitter" => "yyys",
+    "song" => "Maps"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Frankie Cosmos", 
+    "id" => "0x4xCoWaOFd3WsKarzaxnW",
+    "insta" => "frankiecombos", 
+    "twitter" => "frankiecosmos",
+    "song" => "Young"
+  },
+  
+  {
+    "genre" => "indie",
+    "name"=> "Tei Shi", 
+    "id" => "1xcMOgFUM1IYZE22YjCvsL", 
+    "insta" => "teishi", 
+    "twitter" => "TeiShi",
+    "song" => "How Far"
+  },
+
+  {
+    "genre" => "indie",
+    "name"=> "Lana Del Rey", 
+    "id" => "00FQb4jTyendYWaN8pK0wa",
+    "insta" => "lanadelrey", 
+    "twitter" => "lanadelrey",
+    "song" => "Video Games"
+  }
+]
+end
+
+
+
